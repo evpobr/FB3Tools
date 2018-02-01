0599f7ea3f1138c01ba4977e0558d9bee74ce4c8
evpobr <evpobr@gmail.com>
2018-02-01 14:18:43 +0500

    Update file assosiations on install/uninstall

--- WixShellExtension/CustomAction.cs
+++ WixShellExtension/CustomAction.cs
@@ -8,9 +8,18 @@ namespace WixShellExtension
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult NotifyShell(Session session)
+        public static ActionResult NotifyShellAssociationChanged(Session session)
         {
-            session.Log("Begin NotifyShell");
+            try
+            {
+                session.Log("Begin NotifyShell");
+                NativeMethods.SHChangeNotify(SHCNE.SHCNE_ASSOCCHANGED, 0, IntPtr.Zero, IntPtr.Zero);
+            }
+            catch (Exception ex)
+            {
+                session.Log("ERROR in custom action ConfigureEwsFilter {0}", ex.ToString());
+                return ActionResult.Failure;
+            }
 
             return ActionResult.Success;
         }
--- WixShellExtension/NativeMethods.cs
+++ WixShellExtension/NativeMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,21 +9,12 @@ namespace WixShellExtension
 {
     internal enum SHCNE: uint
     {
-        SHCNE_ASSOCCHANGED,
-        SHCNE_ATTRIBUTES,
-        SHCNE_CREATE,
-        SHCNE_DELETE,
-        SHCNE_DRIVEADD,
-        SHCNE_DRIVEADDGUI,
-        SHCNE_DRIVEREMOVED,
-        SHCNE_EXTENDED_EVENT,
-        SHCNE_FREESPACE,
-
+        SHCNE_ASSOCCHANGED = 0x08000000
     }
 
     internal static class NativeMethods
     {
         [DllImport("shell32.dll", CharSet = CharSet.Auto)]
-        internal static void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+        internal static extern void SHChangeNotify(SHCNE wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
     }
 }
